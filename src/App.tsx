39741b276318adb2788cc66a9379384125e347f4
Victor Ferri <victorferri_@hotmail.com>
2021-08-05 12:40:14 -0300

    Update MainAndAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Button } from './components/Button'
+import { Home } from './pages/Home';
 
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <Home />
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImage from '../assets/images/logo.svg'
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImage from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss'
 
 export function Home() {
     return (
-        <div>
+        <div id="pageAuth">
             <aside>
                 <img src={illustrationImg} alt="Illustração simbolizando pergunstas e respostas." />
                 <strong>Crie Salas de Q&amp;A ao-vivo!</strong>
@@ -11,8 +14,22 @@ export function Home() {
             </aside>
 
             <main>
-                <div>
-                    <img src={logoImage} alt="" /> 
+                <div className="mainContent">
+                    <img src={logoImage} alt="letMeAsk" />
+                    <button>
+                        <img src={googleIconImg} alt="" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>Ou entre em uma sala</div>
+                    <form>
+                        <input
+                        
+                        type="text"
+                        placeholder="Digite o código da sala"
+
+                        />
+                        <button type="submit">Entrar na sala</button>
+                    </form>
                 </div>
             </main>
         </div>
