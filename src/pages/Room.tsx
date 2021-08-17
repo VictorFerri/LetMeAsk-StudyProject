9e07cdb8682c4bd911e369bada5fca35e5a18d61
Victor Ferri <victorferri_@hotmail.com>
2021-08-17 11:21:44 -0300

    DpsEuMexo

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
 import '../styles/room.scss';
 
 type RoomParams = {
@@ -10,14 +12,25 @@ type RoomParams = {
 }
 
 export function Room() {
+    const user = useAuth();
     const params = useParams<RoomParams>();
+    const [newQuestion, setNewQuestion] = useState('');
+
+    const roomId = params.id;
+
+    function handleSendQuestion() {
+        if (newQuestion.trim() == '') {
+            return;
+        }
+
+    }
     
     return (
         <div id="pageRoom">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="LetMeAsk" />
-                    <RoomCode code={params.id}/>
+                    <RoomCode code={roomId}/>
                 </div>
             </header>
 
@@ -30,6 +43,8 @@ export function Room() {
                 <form>
                     <textarea 
                         placeholder="O que você quer perguntar?"
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
                     />
 
                     <div className="formFooter">
