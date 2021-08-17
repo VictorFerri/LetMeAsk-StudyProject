cb12f7086d4506af81364fe8b8d4aacd34bf7f9b
Victor Ferri <victorferri_@hotmail.com>
2021-08-17 11:11:15 -0300

    Update AddRoom

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,22 @@
+import copyImg from '../assets/images/copy.svg';
+
+import '../styles/roomCode.scss';
+
+type RoomCodeProps = {
+    code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText(props.code);
+    }
+    return (
+        <button className="roomCode" onClick={copyRoomCodeToClipboard}>
+            <div>
+                <img src={copyImg} alt="Copy room code" />
+            </div>
+            <span>Sala #{props.code}</span>
+        </button> 
+    );
+}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,43 @@
+import { useParams } from 'react-router-dom';
+
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import '../styles/room.scss';
+
+type RoomParams = {
+    id: string;
+}
+
 export function Room() {
+    const params = useParams<RoomParams>();
+    
     return (
-        <h1>Sala</h1>
+        <div id="pageRoom">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="LetMeAsk" />
+                    <RoomCode code={params.id}/>
+                </div>
+            </header>
+
+            <main>
+                <div className="roomTitle">
+                    <h2>Sala React</h2>
+                    <span>4 Perguntas</span>
+                </div>
+
+                <form>
+                    <textarea 
+                        placeholder="O que você quer perguntar?"
+                    />
+
+                    <div className="formFooter">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button type="submit" >Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     );
 }
